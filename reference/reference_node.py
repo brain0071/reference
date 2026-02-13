62d818bda606489f5554b6eb91702bb5ccb53679
brain0071 <18341314091brain@gmail.com>
2026-02-13 17:39:46 +0800

    0213

--- reference/reference_node.py
+++ reference/reference_node.py
@@ -27,9 +27,11 @@ class Reference(Node):
         yaw = 0.0
         att = self.euler_to_quaternion(roll, pitch, yaw)
             
-        x, y, z = 1.0 * math.cos(deg), 1.0 * math.sin(deg), 0.2
+        #x, y, z = 1.0 * math.cos(deg), 1.0 * math.sin(deg), 0.2
+        x, y, z = -0.3, 0.3, 0.3
         pos = [x, y, z]
-        vel = [(x - self.last_pos[0]) / self.dt , (y - self.last_pos[1]) / self.dt, (z - self.last_pos[2]) / self.dt]
+        #vel = [(x - self.last_pos[0]) / self.dt , (y - self.last_pos[1]) / self.dt, (z - self.last_pos[2]) / self.dt]
+        vel = [0.0, 0.0, 0.0]
         rate = [0.0, 0.0, 0.0]
         self.last_pos = pos
   
